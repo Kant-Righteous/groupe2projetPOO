4d6686ade297e410f6a774559f44a4698e6a11bf
Zhengyi Chen <chenzhengyifr@gmail.com>
2026-01-05 20:29:55 +0100

    init: spring boot project setup

---
+++ src/main/java/fr/miage/groupe2projetpoo/Groupe2projetPooApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.groupe2projetpoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groupe2projetPooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Groupe2projetPooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/miage/groupe2projetpoo/Groupe2projetPooApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.groupe2projetpoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Groupe2projetPooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
