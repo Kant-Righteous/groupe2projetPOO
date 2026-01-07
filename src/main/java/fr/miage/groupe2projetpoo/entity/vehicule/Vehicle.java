1a0b317ed987acd77ca3e145470f09c319ae2bd5
arijh <arijh43@gmail.com>
2026-01-07 15:24:01 +0100

    feat:base de la classe Vehicule

--- src/main/java/fr/miage/groupe2projetpoo/entity/vehicule/Vehicle.java
+++ src/main/java/fr/miage/groupe2projetpoo/entity/vehicule/Vehicle.java
@@ -1,5 +1,52 @@
 package fr.miage.groupe2projetpoo.entity.vehicule;
 
 public abstract class Vehicle {
+    //Propriétés
+    private int idVehicule;
+    private String typeVehicule;
+    private String marqueVehicule;
+    private String couleurVehicule;
+    private String modeleVehicule;
+    private String villeVehicule;
 
+    // Getters
+    public int getIdVehicule() {
+        return idVehicule;
+    }
+    public String getTypeVehicule() {
+        return typeVehicule;
+    }
+    public String getMarqueVehicule() {
+        return marqueVehicule;
+    }
+    public String getCouleurVehicule() {
+        return couleurVehicule;
+    }
+    public String getModeleVehicule() {
+        return modeleVehicule;
+    }
+    public String getVilleVehicule() {
+        return villeVehicule;
+    }
+
+
+    // Setters
+    public void setIdVehicule(int idV) {
+        this.idVehicule = idV;
+    }
+    public void setTypeVehicule(String type) {
+        this.typeVehicule = type;
+    }
+    public void setMarqueVehicule(String marque) {
+        this.marqueVehicule = marque;
+    }
+    public void setCouleurVehicule(String couleur) {
+        this.couleurVehicule = couleur;
+    }
+    public void setModeleVehicule(String modele) {
+        this.modeleVehicule = modele;
+    }
+    public void setVilleVehicule(String ville) {
+        this.villeVehicule = ville;
+    }
 }
