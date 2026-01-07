cabf1c46835afe2730d7aad28f0aeab394c139f4
mamad <mamadousalioukarim24@gmail.com>
2026-01-07 16:26:39 +0100

    feat: add Assurance entity class with id, name, option, and tariff fields

--- src/main/java/fr/miage/groupe2projetpoo/entity/assurance/Assurance.java
+++ src/main/java/fr/miage/groupe2projetpoo/entity/assurance/Assurance.java
@@ -1,5 +1,7 @@
 package fr.miage.groupe2projetpoo.entity.assurance;
 
+import java.util.Objects;
+
 public class Assurance {
 
     private int idA;
@@ -49,5 +51,31 @@ public class Assurance {
 
     public void setTarif(float tarif) {
         this.tarif = tarif;
+    }
+
+    @Override
+    public String toString() {
+        return "Assurance{" +
+                "idA=" + idA +
+                ", nom='" + nom + '\'' +
+                ", option='" + option + '\'' +
+                ", tarif=" + tarif +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Assurance assurance = (Assurance) o;
+        return idA == assurance.idA && Float.compare(assurance.tarif, tarif) == 0 && Objects.equals(nom, assurance.nom)
+                && Objects.equals(option, assurance.option);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idA, nom, option, tarif);
     }
 }
