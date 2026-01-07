3b6b1f7142719b86ceaf5fb8063d247d9191253f
arijh <arijh43@gmail.com>
2026-01-07 15:39:04 +0100

    feat: ajoute du constructeur de vehicule + les constructeur des fils du vehicule

--- src/main/java/fr/miage/groupe2projetpoo/entity/vehicule/Moto.java
+++ src/main/java/fr/miage/groupe2projetpoo/entity/vehicule/Moto.java
@@ -1,5 +1,7 @@
 package fr.miage.groupe2projetpoo.entity.vehicule;
 
 public class Moto extends Vehicle {
-
+    public Moto(int idVehicule, String typeVehicule, String marqueVehicule, String couleurVehicule, String modeleVehicule, String villeVehicule) {
+        super(idVehicule, typeVehicule, marqueVehicule, couleurVehicule, modeleVehicule, villeVehicule);
+    }
 }
