7c261d4de797d6e2574a0ee3a2e5bc205eca8d80
arijh <arijh43@gmail.com>
2026-01-08 13:45:41 +0100

    feat: travailes sur controller repository et service et y a d'autre modification de proprietes et nom de methode ou autre

--- src/main/java/fr/miage/groupe2projetpoo/service/VehicleService.java
+++ src/main/java/fr/miage/groupe2projetpoo/service/VehicleService.java
@@ -1,8 +1,45 @@
 package fr.miage.groupe2projetpoo.service;
 
+import fr.miage.groupe2projetpoo.entity.utilisateur.*;
+import fr.miage.groupe2projetpoo.entity.vehicule.Vehicle;
+import fr.miage.groupe2projetpoo.entity.vehicule.Voiture;
+import fr.miage.groupe2projetpoo.repository.UserRepository;
+import fr.miage.groupe2projetpoo.repository.VehicleRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class VehicleService {
+    /*calculer le prix total d’une location
 
+    vérifier la disponibilité
+
+    appliquer des promotions ou commissions*/
+
+    // Propriétés
+    private final VehicleRepository vehiculeRepository;
+
+    //Constructeur
+    public VehicleService(VehicleRepository vehiculeRepository) {
+        this.vehiculeRepository = vehiculeRepository;
+    }
+
+    //Methodes
+    /**
+     * ajout d'un vehicule
+     */
+    public Vehicle addVehicule(String idVehicule, String typeVehicule, String marqueVehicule,
+                            String couleurVehicule, String modeleVehicule, String villeVehicules, boolean estDisponible, double prixVehiculeParJour) {
+        if (vehiculeRepository.existsById(idVehicule)) {
+            return null;
+        }
+
+        Vehicle vehicule;
+        vehicule = new Voiture(idVehicule, typeVehicule, marqueVehicule, couleurVehicule,modeleVehicule,villeVehicules, estDisponible,60,null,null);
+
+
+        return vehiculeRepository.save(vehicule);
+    }
 }
